c60a774149f147f09a8fd3e468a1891cd9ec69e5
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 14:47:52 +0700

    add

--- app/src/main/java/superheroku/id/co/androidkotlin3countnumber/MainActivity.kt
+++ app/src/main/java/superheroku/id/co/androidkotlin3countnumber/MainActivity.kt
@@ -2,6 +2,9 @@ package superheroku.id.co.androidkotlin3countnumber
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe (view: View) {
+        // Get the text view
+        val showCountTextView = findViewById(R.id.txtNumber) as TextView
+
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString();
+    }
+
 }
