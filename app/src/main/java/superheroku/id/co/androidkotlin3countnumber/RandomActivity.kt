71b329b62819108c9babf4450b92f5507aba9a6f
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 14:51:52 +0700

    add

---
+++ app/src/main/java/superheroku/id/co/androidkotlin3countnumber/RandomActivity.kt
@@ -0,0 +1,12 @@
+package superheroku.id.co.androidkotlin3countnumber
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random)
+    }
+}
